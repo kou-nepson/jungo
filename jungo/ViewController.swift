5fb7c2a300cda5cf01c78eb490d790dbcf6196de
杉山航 <cjgjc803@gmail.com>
2018-05-20 12:13:29 +0900

    5X5に変更

--- jungo/ViewController.swift
+++ jungo/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var turn_count: Int = 1
+    var masuArray: [Int] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    var uk_cont: [Int] = [0,0,0,0,0]
+    @IBOutlet var hyouji_koma: [UILabel]!
+    @IBOutlet var ukcountlabel: [UILabel]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func masu(sender: AnyObject) {
+        if turn_count > 5 {
+            turn_count = 1
+        }
+        self.hantei(sender: sender.tag)
+        if masuArray[sender.tag] == 0 {
+            masuArray[sender.tag] = turn_count
+            hyouji_koma[sender.tag].text = String(turn_count)
+            turn_count += 1
+        }
+        
+        
+        Swift.print("\nturn_count\(turn_count)\nマスの状態\(masuArray[sender.tag])\nsendertag\(sender.tag)")
+    }
+    
+    func  hantei(sender: Int){
+        if sender > 7{
+            if masuArray[sender] == masuArray[sender-8] && masuArray[sender-4] != turn_count {
+                masuArray[sender-4] = 0
+                hyouji_koma[sender-4].text = ""
+                uk_cont[turn_count-1] += 1
+                ukcountlabel[turn_count-1].text = String(uk_cont[turn_count-1])
+                
+            }
+        }
+        if sender < 7 {
+            if masuArray[sender] == masuArray[sender+8] && masuArray[sender+4] != turn_count{
+                masuArray[sender+4] = 0
+                hyouji_koma[sender+4].text = ""
+            }
+        }
+        
+        
+        
+    }
+    
 }
 
