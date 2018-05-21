befca78c953835c1c04926a1d45c097b4c0d4b79
杉山航 <cjgjc803@gmail.com>
2018-05-21 14:52:48 +0900

    先手ターンと後手ターンをif文じょうで分ける数式を実装

--- jungo/ViewController.swift
+++ jungo/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var turn_count: Int = 1
-    var masuArray: [Int] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    var turn_count: [Int] = [1,1]
+    var masuArray: [Int] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     var uk_cont: [Int] = [0,0,0,0,0]
+    var ok_count: [Int] = [0,0,0,0,0]
+    @IBOutlet var okcountlabel: [UILabel]!
     @IBOutlet var hyouji_koma: [UILabel]!
     @IBOutlet var ukcountlabel: [UILabel]!
 
@@ -27,14 +29,23 @@ class ViewController: UIViewController {
     }
 
     @IBAction func masu(sender: AnyObject) {
-        if turn_count > 5 {
-            turn_count = 1
+        if turn_count[0] > 5 {
+            turn_count[0] = 1
+            turn_count[1] += 1
+        }
+        //先手ターンの判定条件
+        if (turn_count[0] + turn_count[1]) % 2 == 0{
+            
+        }
+        //後手ターンの判定条件
+        if (turn_count[0] + turn_count[1]) % 2 == 1{
+            
         }
         self.hantei(sender: sender.tag)
         if masuArray[sender.tag] == 0 {
-            masuArray[sender.tag] = turn_count
-            hyouji_koma[sender.tag].text = String(turn_count)
-            turn_count += 1
+            masuArray[sender.tag] = turn_count[0]
+            hyouji_koma[sender.tag].text = String(turn_count[0])
+            turn_count[0] += 1
         }
         
         
@@ -42,24 +53,39 @@ class ViewController: UIViewController {
     }
     
     func  hantei(sender: Int){
-        if sender > 7{
-            if masuArray[sender] == masuArray[sender-8] && masuArray[sender-5] != turn_count {
+        if sender > 12{
+            if masuArray[sender] == masuArray[sender-10] && masuArray[sender-5] != turn_count[0] {
+                Swift.print("とりまいふ分通った！")
                 masuArray[sender-5] = 0
                 hyouji_koma[sender-5].text = ""
-                uk_cont[turn_count-1] += 1
-                ukcountlabel[turn_count-1].text = String(uk_cont[turn_count-1])
-                
+                self.komapoint()
             }
         }
-        if sender < 7 {
-            if masuArray[sender] == masuArray[sender+8] && masuArray[sender+5] != turn_count{
+        if sender < 12 {
+            if masuArray[sender] == masuArray[sender+10] && masuArray[sender+5] != turn_count[0]{
+                Swift.print("とりまいふ分通った！")
                 masuArray[sender+5] = 0
                 hyouji_koma[sender+5].text = ""
+                self.komapoint()
             }
         }
         
         
         
+    }
+    
+    func komapoint(){
+        
+        //先手ターンの判定条件
+        if (turn_count[0] + turn_count[1]) % 2 == 0{
+            ok_count[turn_count[0]-1] += 1
+            okcountlabel[turn_count[0]-1].text = String(ok_count[turn_count[0]-1])
+        }
+        //後手ターンの判定条件
+        if (turn_count[0] + turn_count[1]) % 2 == 1{
+            uk_cont[turn_count[0]-1] += 1
+            ukcountlabel[turn_count[0]-1].text = String(uk_cont[turn_count[0]-1])
+        }
     }
     
 }
