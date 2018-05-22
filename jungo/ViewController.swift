a8e2c9b823be52d776620a34c29b6dd622769c35
杉山航 <cjgjc803@gmail.com>
2018-05-22 16:46:14 +0900

    まだ未完成だが横一列の左のコマのみが取れるようになった（未確認だが ）

--- jungo/ViewController.swift
+++ jungo/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     var masuArray: [Int] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     var uk_cont: [Int] = [0,0,0,0,0]
     var ok_count: [Int] = [0,0,0,0,0]
+    var n: Int = 0
+    var m: Int = 0
+    var j: Int = 0
+    var k: Int = 0
     @IBOutlet var okcountlabel: [UILabel]!
     @IBOutlet var hyouji_koma: [UILabel]!
     @IBOutlet var ukcountlabel: [UILabel]!
@@ -33,13 +37,16 @@ class ViewController: UIViewController {
             turn_count[0] = 1
             turn_count[1] += 1
         }
+        self.koma_get(tag: Int(sender as! NSNumber))
         //先手ターンの判定条件
         if (turn_count[0] + turn_count[1]) % 2 == 0{
-            
+            ok_count[turn_count[0]-1] += 1
+            okcountlabel[turn_count[0]-1].text = String(ok_count[turn_count[0]-1])
         }
         //後手ターンの判定条件
         if (turn_count[0] + turn_count[1]) % 2 == 1{
-            
+            uk_cont[turn_count[0]-1] += 1
+            ukcountlabel[turn_count[0]-1].text = String(uk_cont[turn_count[0]-1])
         }
         self.hantei(sender: sender.tag)
         if masuArray[sender.tag] == 0 {
@@ -76,17 +83,37 @@ class ViewController: UIViewController {
     
     func komapoint(){
         
-        //先手ターンの判定条件
+        //先手が差したコマかどうかを見分ける式
         if (turn_count[0] + turn_count[1]) % 2 == 0{
             ok_count[turn_count[0]-1] += 1
             okcountlabel[turn_count[0]-1].text = String(ok_count[turn_count[0]-1])
         }
-        //後手ターンの判定条件
+        //後手が刺したコマかどうかを見分ける式
         if (turn_count[0] + turn_count[1]) % 2 == 1{
             uk_cont[turn_count[0]-1] += 1
             ukcountlabel[turn_count[0]-1].text = String(uk_cont[turn_count[0]-1])
         }
     }
     
+    func koma_get(tag: Int) {
+        //盤上の駒の横
+        m = tag % 5
+        //盤上の駒の縦
+        n = (tag - m)/5
+        //左しか取れないコード
+        for i in 0...m{
+            if masuArray[tag-i] != 0{
+                if masuArray[tag-i] == turn_count[0]{
+                    for k in 1...j-1{
+                        hyouji_koma[tag-k].text = ""
+                        
+                    }
+                }
+            }
+            
+            j += 1
+        }
+        
+    }
 }
 
